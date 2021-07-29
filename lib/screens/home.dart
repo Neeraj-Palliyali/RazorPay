51ac4ba00e8a5c6310022ff41562af6a0bb4bd2c
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2021-07-30 01:15:36 +0530

    Razor integration

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,33 +9,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController textEditingController = new TextEditingController();
 
-  late Razorpay razorpay;
+  late Razorpay _razorpay;
 
   @override
   void initState() {
     super.initState();
-    razorpay = new Razorpay();
+    _razorpay = Razorpay();
 
-    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
-    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
-    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
-    
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
   }
 
   @override
   void dispose() {
-    
     super.dispose();
-    razorpay.clear();
+    _razorpay.clear();
   }
 
-  void handlerPaymentSuccess(){
-
+  void _handlePaymentSuccess() {
+    print("PaymentSuccesful");
   }
 
-  void handlerErrorFailure(){}
-  void handlerExternalWallet(){}
+  void openCheckout() {
+    var options = {
+  'key': 'rzp_test_R4mjU1unUMgSQU',
+  'amount': num.parse(textEditingController.text)*100,
+  'name': 'Acme Corp.',
+  'description': 'Fine T-Shirt',
+  'prefill': {
+    'contact': '8888888888',
+    'email': 'me@gmail.com'
+    }
+  };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _handlePaymentError() {
+    print(("Payment Error"));
+  }
+
+  void _handleExternalWallet() {
+    print("External Wallet");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,12 +67,35 @@ class _HomeState extends State<Home> {
         title: Text("Razor Pay"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextField(),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: 200,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  controller: textEditingController,
+                  decoration: InputDecoration(
+                    hintText: "Enter the amount",
+                  ),
+                ),
+              ),
+            ],
+          ),
           SizedBox(
             height: 12,
           ),
-          ElevatedButton(onPressed: () => {}, child: Text("Pay Now")),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Colors.amber),
+              onPressed: () { openCheckout();},
+              child: Text("Pay Now")),
         ],
       ),
     );
